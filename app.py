981dacebd940a8b21d3e1adf558c9854ea470bdc
Priyanka <piyu19@gmail.com>
2024-09-25 23:01:23 -0700

    Add files via upload
    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+import chainlit as cl
+from movie_functions import get_now_playing_movies, get_showtimes, buy_ticket
+
+load_dotenv()
+
+# Note: If switching to LangSmith, uncomment the following, and replace @observe with @traceable
+# from langsmith.wrappers import wrap_openai
+# from langsmith import traceable
+# client = wrap_openai(openai.AsyncClient())
+
+from langfuse.decorators import observe
+from langfuse.openai import AsyncOpenAI
+
+client = AsyncOpenAI()
+
+gen_kwargs = {
+    "model": "gpt-4o",
+    "temperature": 0.2,
+    "max_tokens": 500
+}
+
+SYSTEM_PROMPT = """\
+You are a virtual assistant who has the capability to provide a user information about movies, showtimes and possible help them book tickets.
+
+For list current movies requests, use the following function call format:
+{"function": "get_now_playing_movies", "parameters": {}}
+
+To get showtimes for a movie, use the following function call format:
+{"function": "get_showtimes", "parameters": {title, location}}
+
+After receiving the results of a function call, incorporate that information into your response to the user.
+"""
+
+@observe
+@cl.on_chat_start
+def on_chat_start():
+    message_history = [{"role": "system", "content": SYSTEM_PROMPT}]
+    cl.user_session.set("message_history", message_history)
+
+@observe
+async def generate_response(client, message_history, gen_kwargs):
+    response_message = cl.Message(content="")
+    await response_message.send()
+
+    stream = await client.chat.completions.create(messages=message_history, stream=True, **gen_kwargs)
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
+
+    await response_message.update()
+
+    return response_message
+
+@cl.on_message
+@observe
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
+
+    response_message = await generate_response(client, message_history, gen_kwargs)
+
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
+
+if __name__ == "__main__":
+    cl.main()
