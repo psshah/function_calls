11fe05c41614d805550d6a21a3e67886ae3671c5
Priyanka Shah <psshah@uber.com>
2024-09-28 17:26:41 -0700

    Add milestone 6 - use RAG to fetch movie reviews.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import chainlit as cl
+from prompts import SYSTEM_PROMPT, REVIEW_SYSTEM_PROMPT
 from movie_functions import get_now_playing_movies, get_showtimes, buy_ticket, get_reviews, confirm_ticket_purchase
 import json
 import re
@@ -21,24 +22,6 @@ gen_kwargs = {
     "temperature": 0.2,
     "max_tokens": 500
 }
-
-SYSTEM_PROMPT = """\
-You are a virtual assistant who has the capability to provide a user information about movies, showtimes and possible help them book tickets.
-
-For list current movies requests, use the following function call format:
-{"function": "get_now_playing_movies", "parameters": {}}
-
-To get showtimes for a movie, use the following function call format:
-{"function": "get_showtimes", "parameters": {"title": "title", "location": "location"}}
-
-If the user indicates they want to buy a ticket, use the following function call format to confirm the details first:
-{"function": "buy_ticket", "parameters": {"theater": "theater", "movie": "movie", "showtime": "showtime"}}
-
-If the user confirms they want to proceed with the purchase, use the following function to complete the ticket purchase:
-{"function": "confirm_ticket_purchase", "parameters": {"theater": "theater", "movie": "movie", "showtime": "showtime"}}
-
-After receiving the results of a function call, incorporate that information into your response to the user.
-"""
 
 @observe
 @cl.on_chat_start
@@ -65,10 +48,10 @@ def parse_json(response_message):
         json_pattern = r'\{[^{}]*\}'
 
         # Find the JSON object in the input string
-        match = re.search(json_pattern, response_message.content)
+        match = re.search(json_pattern, response_message)
         if match:
             json_string = match.group()
-            #print("json is " + json_string)
+            print("json is " + json_string)
 
             # Parse the JSON string
             data = json.loads(json_string)
@@ -82,14 +65,41 @@ def parse_json(response_message):
         print("Required keys not found in the JSON data")
     return None
 
+
+async def fetch_reviews(message_history):
+    temp_message_history = message_history.copy()
+    temp_message_history[0] = {"role": "system", "content": REVIEW_SYSTEM_PROMPT}
+    temp_message_history.append({"role": "system", "content": REVIEW_SYSTEM_PROMPT})
+
+    response_message = await client.chat.completions.create(messages=temp_message_history, **gen_kwargs)
+
+    response_message = response_message.choices[0].message.content
+
+    #response_message = await generate_response(client, message_history, gen_kwargs)
+    print("Review response is " + response_message)
+    data = parse_json(response_message)
+    if data:
+        movie_id = data['id']
+        print(f"movie id: {movie_id}")
+        if movie_id:
+            reviews = get_reviews(movie_id)
+            message_history.append({"role": "system", "content": rf"CONTEXT: {reviews}"})
+        else:
+            print("Missing movie id for get_reviews")
+
 @cl.on_message
 @observe
 async def on_message(message: cl.Message):
     message_history = cl.user_session.get("message_history", [])
     message_history.append({"role": "user", "content": message.content})
 
+    await fetch_reviews(message_history)
+
     response_message = await generate_response(client, message_history, gen_kwargs)
     #print(response_message.content)
+
+    message_history.append({"role": "assistant", "content": response_message.content});
+    cl.user_session.set("message_history", message_history)
 
     # Check if the response contains get_now_playing_movies
     function_called = True
@@ -106,7 +116,7 @@ async def on_message(message: cl.Message):
         elif "get_showtimes" in response_message.content:
             function_called = True
             # Extract title and location from the parameters
-            data = parse_json(response_message)
+            data = parse_json(response_message.content)
             if data:
                 title = data['title']
                 location = data['location']
@@ -120,7 +130,7 @@ async def on_message(message: cl.Message):
                     print("Missing title or location for get_showtimes")
         elif "buy_ticket" in response_message.content:
             function_called = True
-            data = parse_json(response_message)
+            data = parse_json(response_message.content)
             if data:
                 # Extract title and location from the parameters
                 theater = data['theater']
@@ -136,7 +146,7 @@ async def on_message(message: cl.Message):
                     print("Missing theater, movie or showtime for buy_ticket")
         elif "confirm_ticket_purchase" in response_message.content:
             function_called = True
-            data = parse_json(response_message)
+            data = parse_json(response_message.content)
             if data:
                 # Extract title and location from the parameters
                 theater = data['theater']
